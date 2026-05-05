89c8d4588d171a7e72e3034e02426de57a060662
MomoElla <uryarya6786@gmail.com>
2026-05-05 18:41:43 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const nav = document.querySelector(".nav");
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+function showVilla(id) {
+  const contents = document.querySelectorAll(".villa-content");
+  const tabs = document.querySelectorAll(".tab");
+
+  contents.forEach(c => c.classList.remove("active"));
+  tabs.forEach(t => t.classList.remove("active"));
+
+  document.getElementById(id).classList.add("active");
+  event.target.classList.add("active");
+}
